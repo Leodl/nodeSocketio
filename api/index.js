b9afe1cca38c1456992aa3a41e8698996f166923
Leodl <1215918846@qq.com>
2018-10-10 16:05:01 +0800

    新增注释

--- api/index.js
+++ api/index.js
@@ -10,14 +10,16 @@ app.use(express.static(path.join(__dirname,'../', 'web'),{maxAge:1000*60*60}));
 // app.get('/', function(req, res){
 //   res.send('<h1>Hello world</h1>');
 // });
-io.on('connection', function(socket){
+io.on('connection', function(socket){  //socket连接成功触发
+	console.log(io)
+	console.log("连接")
  // if (io.sockets.connected[socket.id]) {
  //        io.sockets.connected[socket.id].emit('chat message', msg);
  //    }
 
   socket.on('chat message', function(msg){
   	 //io.to(socket.id).emit('chat message', msg);
-     io.emit('chat message', msg);
+     io.emit('chat message', msg);  //服务器接收消息并发送给客户端
   });
 
 });
